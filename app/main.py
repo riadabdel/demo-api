30f0a4e06032980da475896008c53ab539ecc59e
Riad Abdelouhab <wowriad@gmail.com>
2026-07-31 12:32:29 +0000

    Release v7

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ def home():
 
     return {
         "application": "Kubernetes Demo",
-        "version": "v4",
+        "version": "v7",
         "pod": socket.gethostname(),
         "database": "connected",
         "visits": count
